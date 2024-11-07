e794209f11dc444e9498ee0a8a8a5d5d414acb04
JuanJL217 <juandocumentos217@gmail.com>
2024-11-07 17:25:22 -0300

    Haciendo Pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,156 @@
 #include "pa2m.h"
+#include "src/hash.h"
 
 void prueba_simple()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i == j, "i es igual a j (%d == %d)", i, j);
-	pa2m_afirmar(2*i == 10, "2*i es igual a 2 (%d)", 2*i);
+	hash_t* hash1 = hash_crear(0);
+	if (!hash1) {
+		printf("Error al crear el primer Hash");
+		return;
+	}
+	pa2m_afirmar(hash1 != NULL, "Hash con capacidad menor a 3");
+	hash_t* hash2 = hash_crear(3);
+	if (!hash2) {
+		printf("Error al crear el segundo Hash");
+		hash_destruir_todo(hash1, NULL);
+		return;
+	}
+	pa2m_afirmar(hash2 != NULL, "Hash con capacidad igual a 3");
+	hash_t* hash3 = hash_crear(10);
+	if (!hash3) {
+		printf("Error al crear el tercer Hash");
+		hash_destruir_todo(hash1, NULL);
+		hash_destruir(hash2);
+		return;
+	}
+	pa2m_afirmar(hash3 != NULL, "Hash con capacidad mayor a 3");
+
+	hash_destruir(hash1);
+	hash_destruir(hash2);
+	hash_destruir(hash3);
+}
+
+void prueba_insertar_elemento()
+{
+	hash_t* hash = hash_crear(15);
+	if (!hash) {
+		printf("Error al crear el diccionario");
+		return;
+	}
+	pa2m_afirmar(hash != NULL, "Nuevo Hash creado");
+	char* clave1 = "Juan";
+	size_t edad  = 23;
+	if (!hash_insertar(hash, clave1, (void*)&edad, NULL)) {
+		printf("No se puedo insertar el par clave-valor: Juan-23");
+		hash_destruir(hash);
+		return;
+	}
+	char* direccion_diferente = "Juan";
+	char* otro_nombre = "Ernesto";
+	pa2m_afirmar(hash_cantidad(hash) == 1, "Hay un par en el Hash");
+	pa2m_afirmar(hash_contiene(hash, direccion_diferente) == true, "La clave guardada es Juan");
+	size_t* obtener = (size_t*)hash_buscar(hash, direccion_diferente);
+	size_t* obtener_otra_clave = (size_t*)hash_buscar(hash, otro_nombre);
+	pa2m_afirmar(obtener == &edad, "Su valor es '%li' Direccion obtenida: '%p' -> direccion esperada: '%p'", *obtener, obtener, &edad);
+	pa2m_afirmar(obtener_otra_clave == NULL, "No hay ninguna clave 'Ernesto' en el hash");
+	hash_destruir_todo(hash, NULL);
+}
+
+void prueba_insertar_varios_elementos()
+{
+	hash_t* hash = hash_crear(20);
+	if (!hash) {
+		printf("Error al crear el diccionario");
+		return;
+	}
+	pa2m_afirmar(hash != NULL, "Nuevo Hash creado");
+	char* listado[6] = {"Juan", "Ernesto", "Juarez", "Lezama", "Pepito", "Leonardo"};
+	size_t numeros[6] = {4, 7, 6, 6, 1, 8};
+	for (size_t i = 0; i < 6; i ++) {
+		if(!hash_insertar(hash, listado[i], (void*)&numeros[i], NULL)) {
+        	printf("Error al insertar el par clave-valor: %s-%li", listado[i], numeros[i]);
+			hash_destruir(hash);
+			return;
+		}
+	}
+	pa2m_afirmar(hash_cantidad(hash) == 6, "Hay 4 pares en el hash");
+
+	for (size_t j = 0; j < 6; j ++) {
+		pa2m_afirmar(hash_contiene(hash, listado[j]) == true, "Clave '%s' encontrada", listado[j]);
+		size_t* obtener_valor = hash_buscar(hash, listado[j]);
+		pa2m_afirmar(obtener_valor == &numeros[j], "Su valor es  '%li'. Dirección obtenida: '%p' -> dirección esperada: '%p'", *obtener_valor, obtener_valor, &numeros[j]);
+	}
+	hash_destruir_todo(hash, NULL);
+}
+
+void prueba_insertar_elementos_parte_2()
+{
+	hash_t* hash = hash_crear(1);
+	if (!hash) {
+		printf("Error al crear el diccionario");
+		return;
+	}
+	pa2m_afirmar(hash != NULL, "Nuevo Hash creado");
+	char* animales[10] = {"Perro", "Gato", "Elefante"};
+	double peso[10] = {10.25, 3.1, 2700.5};
+	
+	for (size_t i = 0; i < 3; i++) {
+		if(!hash_insertar(hash, animales[i], (void*)&peso[i], NULL)) {
+			printf("Error al insertar el par clave-valor: %s-%f", animales[i], peso[i]);
+			hash_destruir_todo(hash, NULL);
+			return;
+		}
+	}
+	pa2m_afirmar(hash_cantidad(hash) == 3, "Hay 3 pares en el hash");
+	for (size_t j = 0; j < 3; j++) {
+		pa2m_afirmar(hash_contiene(hash, animales[j]) == true, "Clave '%s' encontrada", animales[j]);
+		double* obtener_valor = hash_buscar(hash, animales[j]);
+		pa2m_afirmar(obtener_valor == &peso[j], "Su valor es  '%.2f'. Dirección obtenida: '%p' -> dirección esperada: '%p'", *obtener_valor, obtener_valor, &peso[j]);
+	}
+
+	hash_destruir_todo(hash, NULL);
+}
+
+void prueba_insertar_varios_elementos_parte_2()
+{
+	hash_t* hash = hash_crear(1);
+	if (!hash) {
+		printf("Error al crear el diccionario");
+		return;
+	}
+	pa2m_afirmar(hash != NULL, "Nuevo Hash creado");
+	char* animales[10] = {"Perro", "Gato", "Elefante", "Mono", "Caracol", "Leon", "Tigre", "Koala", "Paloma", "Serpiente"};
+	double peso[10] = {10.25, 3.1, 2700.5, 2.80, 0.05, 150.5, 120.2, 5.0, 0.2, 0.15};
+	
+	for (size_t i = 0; i < 10; i++) {
+		if(!hash_insertar(hash, animales[i], (void*)&peso[i], NULL)) {
+			printf("Error al insertar el par clave-valor: %s-%f", animales[i], peso[i]);
+			hash_destruir_todo(hash, NULL);
+			return;
+		}
+	}
+	pa2m_afirmar(hash_cantidad(hash) == 10, "Hay 10 pares en el hash");
+	for (size_t j = 0; j < 10; j++) {
+		pa2m_afirmar(hash_contiene(hash, animales[j]) == true, "Clave '%s' encontrada", animales[j]);
+		double* obtener_valor = hash_buscar(hash, animales[j]);
+		pa2m_afirmar(obtener_valor == &peso[j], "Su valor es  '%.2f'. Dirección obtenida: '%p' -> dirección esperada: '%p'", *obtener_valor, obtener_valor, &peso[j]);
+	}
+
+	hash_destruir_todo(hash, NULL);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo("============== ??? ===============");
+	pa2m_nuevo_grupo("PRUEBA CREAR HASH");
 	prueba_simple();
+
+	pa2m_nuevo_grupo("PRUEBA INSERTAR EN EL HASH SIN REDIMENSION");
+	prueba_insertar_elemento();
+	prueba_insertar_varios_elementos();
+
+	pa2m_nuevo_grupo("PRUEBA INSERTAR EN EL HASH CON REDIMENSION");
+	prueba_insertar_elementos_parte_2();
+	prueba_insertar_varios_elementos_parte_2();
 
 	return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -1,1 +1,220 @@
 #include "hash.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+const size_t CAPACIDAD_MINIMA = 3;
+const size_t CANTIDAD_MAXIMA_POR_BLOQUE = 7;
+const size_t FACTOR_CRECIMIENTO = 2;
+const double FACTOR_PORCENTAJE_CAPACIDAD = 0.75;
+
+typedef struct nodo_par{
+    char* clave;
+    void* valor;
+    struct nodo_par* siguiente;
+} nodo_par_t;
+
+typedef struct bloque_hash {
+    size_t cantidad_pares;
+    nodo_par_t* par;
+} bloque_hash_t;
+
+struct hash {
+    bloque_hash_t* tabla_hash;
+    size_t capacidad_tabla_hash;
+    size_t cantidad_pares_totales;
+};
+
+unsigned int funcion_hash(const char* clave)
+{
+    unsigned int base = 2166136261U;
+    unsigned int dato = 16777619U;
+    for (size_t i = 0; clave[i] != '\0'; i++) {
+        base ^= (unsigned int) clave[i];
+        base *= dato;
+    }
+    return (unsigned int) base;
+}
+
+hash_t* hash_crear(size_t capacidad_inicial)
+{
+    hash_t* hash = calloc(1, sizeof(hash_t));
+    if (!hash){
+        return NULL;
+    }
+    if (capacidad_inicial <= CAPACIDAD_MINIMA) {
+        hash->capacidad_tabla_hash = CAPACIDAD_MINIMA;
+    } else {
+        hash->capacidad_tabla_hash = capacidad_inicial;
+    }
+    hash->tabla_hash = calloc(hash->capacidad_tabla_hash, sizeof(bloque_hash_t));
+    if (!hash->tabla_hash) {
+        free(hash);
+        return NULL;
+    }
+    return hash;
+}
+
+size_t hash_cantidad(hash_t* hash)
+{
+    return hash->cantidad_pares_totales;
+}
+
+nodo_par_t* crear_par(char* clave, void* valor)
+{
+    nodo_par_t* mi_par = calloc(1, sizeof(nodo_par_t));
+    if (!mi_par)
+        return NULL;
+    char* copia_valor = malloc(strlen(clave) + 1);
+    if (!copia_valor) {
+        free(mi_par);
+        return NULL;
+    }
+    strcpy(copia_valor, clave);
+    mi_par->clave = copia_valor;
+    mi_par->valor = valor;
+    return mi_par;
+}
+
+nodo_par_t** buscar_par(nodo_par_t** nodo_actual, char* clave)
+{
+    if (!*nodo_actual || strcmp((*nodo_actual)->clave, clave) == 0)
+        return nodo_actual;
+    return buscar_par(&(*nodo_actual)->siguiente, clave);
+}
+
+nodo_par_t** obtener_par(bloque_hash_t* tabla_hash, size_t tamaño, char* clave, size_t* posicion)
+{
+    unsigned int hasheo = funcion_hash((const char*)clave);
+    size_t posicion_en_la_tabla = (size_t)((size_t)hasheo%tamaño);
+    if (posicion)
+        *posicion = posicion_en_la_tabla;
+    return buscar_par(&(tabla_hash[posicion_en_la_tabla].par), clave);
+}
+
+void redimensionar_tabla_hash(hash_t* hash)
+{   
+    size_t nueva_capacidad = hash->capacidad_tabla_hash*FACTOR_CRECIMIENTO;
+    bloque_hash_t* nueva_tabla_hash = calloc(nueva_capacidad, sizeof(bloque_hash_t));
+    if (!nueva_tabla_hash)
+        return;
+    for (size_t i = 0; i < hash->capacidad_tabla_hash; i++) {
+        nodo_par_t** par = &(hash->tabla_hash[i].par);
+        while (*par) {
+            size_t posicion_del_bloque;
+            nodo_par_t** nodo_final = obtener_par(nueva_tabla_hash, nueva_capacidad, (*par)->clave, &posicion_del_bloque);
+            nodo_par_t* nodo_guardado = *par;
+            (*par) = (*par)->siguiente;
+            *nodo_final = nodo_guardado;
+            nodo_guardado->siguiente = NULL;
+            nueva_tabla_hash[posicion_del_bloque].cantidad_pares++;
+        }
+    }
+    free(hash->tabla_hash);
+    hash->tabla_hash = nueva_tabla_hash;
+    hash->capacidad_tabla_hash = nueva_capacidad;
+}
+
+bool tope_porcentaje_capacidad(hash_t* hash)
+{
+    return hash->cantidad_pares_totales >= (size_t)((double)hash->capacidad_tabla_hash * FACTOR_PORCENTAJE_CAPACIDAD);
+}
+
+
+bool hash_insertar(hash_t* hash, char* clave, void* valor, void** encontrado)
+{
+    if (!hash || !clave)
+        return false;
+
+    size_t posicion_del_bloque;
+    nodo_par_t** par = obtener_par(hash->tabla_hash, hash->capacidad_tabla_hash, clave, &posicion_del_bloque);
+
+    if (!*par) {
+        nodo_par_t* nuevo_par = crear_par(clave, valor);
+        if (!nuevo_par)
+            return false;
+        (*par) = nuevo_par;
+        hash->cantidad_pares_totales++;
+        size_t pares_en_el_bloque = hash->tabla_hash[posicion_del_bloque].cantidad_pares++;
+        if (pares_en_el_bloque >= CANTIDAD_MAXIMA_POR_BLOQUE || tope_porcentaje_capacidad(hash))
+            redimensionar_tabla_hash(hash);
+    } else {
+        if (encontrado)
+            *encontrado = (*par)->valor;
+        (*par)->valor = valor;
+    }
+
+    return true;
+}
+
+void* hash_buscar(hash_t* hash, char* clave)
+{
+    if (!hash || !clave)
+        return NULL;
+    nodo_par_t** par = obtener_par(hash->tabla_hash, hash->capacidad_tabla_hash, clave, NULL);
+    return (*par)?(*par)->valor:NULL;
+}
+
+bool hash_contiene(hash_t* hash, char* clave)
+{
+    if (!hash || !clave)
+        return false;
+    return *(obtener_par(hash->tabla_hash, hash->capacidad_tabla_hash, clave, NULL));
+}
+
+void* hash_quitar(hash_t* hash, char* clave)
+{
+    if (!hash || !clave)
+        return NULL;
+
+    nodo_par_t** par = obtener_par(hash->tabla_hash, hash->capacidad_tabla_hash, clave, NULL);
+    void* valor_guardado = NULL;
+    if (*par) {
+        valor_guardado = (*par)->valor;
+        nodo_par_t* nodo_quitar = (*par);
+        (*par) = (*par)->siguiente;
+        free(nodo_quitar->clave);
+        free(nodo_quitar);
+    }
+    return valor_guardado;
+}
+
+size_t hash_iterar(hash_t* hash, bool (*f)(char*, void*, void*), void* ctx)
+{
+    if (!hash || !f)
+        return false;
+    size_t contador = 0;
+    for (size_t i = 0; i < hash->capacidad_tabla_hash; i++) {
+        nodo_par_t* par = hash->tabla_hash[i].par;
+        while (par){
+            if (!f(par->clave, par->siguiente, ctx))
+                return contador+1;
+            contador++;
+        }
+    }
+    return hash->cantidad_pares_totales;
+}
+
+void hash_destruir(hash_t* hash)
+{   
+    hash_destruir_todo(hash, NULL);
+}
+
+void hash_destruir_todo(hash_t* hash, void (*destructor)(void*))
+{
+    if (!hash)
+        return;
+    for (size_t i = 0; i < hash->capacidad_tabla_hash; i++) {
+        nodo_par_t* par = hash->tabla_hash[i].par;
+        while (par) {
+            nodo_par_t* par_siguiente = par->siguiente;
+            if (destructor)
+                destructor(par->valor);
+            free(par->clave);
+            free(par);
+            par = par_siguiente;
+        }
+    }
+    free(hash->tabla_hash);
+    free(hash);
+}
